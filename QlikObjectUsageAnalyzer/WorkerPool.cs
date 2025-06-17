de88335cfc8cac6dedcbeab132ff0abb637be50c
Øystein Kolsrud <yko@qlik.com>
2025-06-17 08:04:25 +0200

    Cosmetic changes only.

--- QlikObjectUsageAnalyzer/WorkerPool.cs
+++ QlikObjectUsageAnalyzer/WorkerPool.cs
@@ -2,9 +2,7 @@
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading;
 using System.Threading.Tasks;
-using System.Threading.Tasks.Sources;
 
 namespace QlikObjectUsageAnalyzer
 {
